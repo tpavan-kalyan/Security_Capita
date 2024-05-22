5cd8b5fccdd6bb38985e83531025d0b0917fe791
p1kal <p1kal@DESKTOP-EK3HI17>
2024-05-22 13:29:35 +0530

    SpringBoot Angular Project First Push

---
+++ securitycapita/src/main/java/io/managment/securitycapita/SecuritycapitaApplication.java
@@ -0,0 +1,13 @@
+package io.managment.securitycapita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritycapitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritycapitaApplication.class, args);
+	}
+
+}
---
+++ securitycapita/src/test/java/io/managment/securitycapita/SecuritycapitaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.managment.securitycapita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritycapitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
